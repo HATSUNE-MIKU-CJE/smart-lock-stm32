f05dd25909883e900953d4175b2571b61a4feb4b
HATSUNE-MIKU-CJE <136163328@qq.com>
2026-04-22 22:32:42 +0800

    第一次提交：智能密码锁项目基础框架

--- User/main.c
+++ User/main.c
@@ -1,1 +1,14 @@
 #include "stm32f10x.h"                  // Device header
+#include "Motor.h"
+
+
+int main()
+{
+    Motor_Init();
+    Motor_SetLeftSpeed(50);
+    Motor_SetRightSpeed(0);
+    while (1)
+    {
+
+    }
+}
